6b1384e720d5e17709b82369de3dfe82617db516
gianmarcodonetti <gianmarco.donetti@yahoo.it>
2018-11-03 12:21:26 +0100

    adding version and setup script

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import re
+import os
+import pip
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+with open(os.path.join(os.path.dirname(__file__), 'giammis', 'version.py')) as v_file:
+    VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+
+class OverrideInstall(install):
+    """
+    Emulate sequential install of pip install -r requirements.txt
+    """
+
+    def run(self):
+        # install requirements first
+        install_reqs = parse_requirements(
+            './requirements.txt', session=False)
+        reqs = [str(ir.req) for ir in install_reqs]
+        for req in reqs:
+            pip.main(["install", req])
+
+        install.run(self)
+
+
+config = {
+    'name': 'giammis',
+    'description': 'Base repository for different library functions.',
+    'author': 'G. Donetti',
+    'url': '',
+    'download_url': '',
+    'author_email': '',
+    'version': VERSION,
+    'packages': find_packages(),
+    'scripts': [],
+    'cmdclass': {'install': OverrideInstall},
+    'test_suite': 'nose.collector',
+    'tests_require': ['nose'],
+}
+
+setup(**config)
